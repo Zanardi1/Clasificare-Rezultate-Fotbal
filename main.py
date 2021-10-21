be33d0d608ae82764d18e05dcc9cff33b3fccb45
Bogdan Doicin <alex.zanardi@gmail.com>
2021-10-21 10:25:52 +0300

    Actualizarea bibliotecilor

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import numpy as np
 teams = pd.read_csv("teams.csv")
 results = pd.read_csv("results.csv")
 fixtures = pd.read_csv("fixtures.csv")
-players = pd.read_csv("players.csv")
-startingXI = pd.read_csv("startingXI.csv")
+# players = pd.read_csv("players.csv")
+# startingXI = pd.read_csv("startingXI.csv")
 odds = pd.read_csv("odds.csv")
 
 # It appears that the results data set only provides the number of goals scored by each team in each game.
@@ -29,7 +29,7 @@ results['AwayWin'] = np.where(
     results['AwayScore'] > results['HomeScore'], 1, np.where(
         results['AwayScore'] == results['HomeScore'], 0, 0))
 
-results.head()
+print(results.head())
 
 # It would be helpful to have the team name in the data frame, as opposed to the team ID. We merge results with teams
 # data set to obtain this.
@@ -46,13 +46,13 @@ results.rename(columns={'TeamName': 'AwayTeamName'}, inplace=True)
 # Removing unnecessary variables
 results = results.drop(['HomeScore', 'AwayScore', 'HomeShots', 'AwayShots'], 1)
 
-results
+print(results)
 
 # We also split the results table into seasons 1 & 2
 # Spliting tables by seasons 1 & 2
 season1_results = results[results.SeasonID == 1]
 season2_results = results[results.SeasonID == 2]
 
-season1_results
+print(season1_results)
 
 
